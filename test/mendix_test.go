43b9f287eacd94829ce046c5f89e0e2031d83ffc
Yogendra Sharma <eh.yogendra@gmail.com>
2023-01-24 15:55:31 +0530

    Refactor test :tada:

--- test/mendix_test.go
+++ test/mendix_test.go
@@ -1,10 +1,17 @@
 package mendix_test
 
 import (
+	"encoding/json"
+	"fmt"
 	"testing"
 
 	mendix "github.com/yogendra0sharma/mendix-privatecloud-go-sdk"
 )
+
+func prettyPrint(data interface{}) {
+	dbg, _ := json.MarshalIndent(data, "", "  ")
+	fmt.Println(string(dbg))
+}
 
 func setup(t *testing.T) *mendix.Client {
 	// client is the Gitlab client being tested.
